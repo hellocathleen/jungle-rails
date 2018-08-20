afbd72241c58786177bc5c22515d99eeb897d8e9
Cathleen Melendez <cathleen@melendez.com>
2018-08-20 22:25:40 +0000

    Added authenticate with credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
-  
+  before_validation :downcase_email
+
   has_secure_password
 
-  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    if user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
+
 
 end
