5a1d281a9bb2369cef588d2a5fb3cd999c2da621
Cathleen Melendez <cathleen@melendez.com>
2018-08-21 20:37:05 +0000

    removed commented code

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -20,11 +20,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
     expect(page).to have_css 'article.product', count: 10
-    #puts page.html
     first('article.product').find_link('Details').click
     sleep 3
     # DEBUG / VERIFY
-    
     save_screenshot
     
   end
